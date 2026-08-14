8e6a2eaee0cfbd6f8421c60de827ad7ef29e5082
unknown <tanishque30@gmail.com>
2026-08-14 11:17:42 +0530

    Day 3

---
+++ 2ndYear/Day3.js
@@ -0,0 +1,31 @@
+// let student={
+//     "Name":'Tanishque',
+//     "Roll":2503215300208,
+//      "ID":12345,
+//      "Branch": "AIML",
+// }
+// for(key in student){
+//     console.log(key," ",student[key])
+// }
+//Even and odd program
+// function check(num){
+//     if(num%2===0)
+//         console.log(num," is Even");
+//     else
+//         console.log(num, "is Not Even")
+// }
+// check(12)
+//Calculate Percentage prog
+// function calculatePercentage(maths,eng,chem){
+//     let total=maths+eng+chem
+//     return total*100/300
+// }
+// console.log(calculatePercentage(80,75,90),"%")
+// let arr=[1,3,4,5,4]
+// let sum=0
+// for(i of arr)
+// {
+//     console.log(i)
+//     sum+=i;
+// }
+// console.log(sum)
