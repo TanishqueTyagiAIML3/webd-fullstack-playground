b7c57b858bbcd0697cd97a97c28e7592533cae5d
unknown <tanishque30@gmail.com>
2026-09-11 23:20:15 +0530

    Day 6

--- 2ndYear/Rest.js
+++ 2ndYear/Rest.js
@@ -7,3 +7,12 @@ function number(...numbers)
     console.log(total);
 }
 number(1,23,4)
+ console.log("Sum is ",name("Tanishque Tyagi",1,13,4))
+function name(n,...num){
+    console.log("Hello ",n.slice(0,9))
+    let sum=0;
+    for(let each_val of num){
+        sum=sum+each_val;
+    }
+    return sum
+}
