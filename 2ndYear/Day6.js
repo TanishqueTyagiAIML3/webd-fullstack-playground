b081dfc90bd87011eacec2233f4e700f4fa279bb
unknown <tanishque30@gmail.com>
2026-09-18 11:37:02 +0530

    changes

--- 2ndYear/Day6.js
+++ 2ndYear/Day6.js
@@ -10,20 +10,38 @@
 // }
 // let obj=new num("Tanishque")
 // obj.show()
-class secA{
-    name=""
-    roll_no=0
-    add=""
-    M_no=0
-    constructor(n,r,a,m){
-        this.name=n
-        this.roll_no=r
-        this.add=a
-        this.M_no=m
+// class name{
+//     constructor(){
+//         console.log("Hello")
+//     }
+// }
+// class secA extends name{
+//     name=""
+//     roll_no=0
+//     add=""
+//     M_no=0
+//     constructor(n,r,a,m){
+//         super()
+//         this.name=n
+//         this.roll_no=r
+//         this.add=a
+//         this.M_no=m
+//     }
+//     show=()=>{
+//         console.log(this.name,this.roll_no,this.add,this.M_no)
+//     }
+// }
+// let obj=new secA("Tanishque",59,"L-374 Sec-23",9310300737)
+// obj.show()
+class tanishque{
+    static count=0
+    constructor(){
+       tanishque.count=tanishque.count+1
     }
     show=()=>{
-        console.log(this.name,this.roll_no,this.add,this.M_no)
+        console.log(tanishque.count)
     }
 }
-let obj=new secA("Tanishque",59,"L-374 Sec-23",9310300737)
+let obj=new tanishque()
+let obj2=new tanishque()
 obj.show()
