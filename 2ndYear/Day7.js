0d8adc4f842d2cf1fc491e1afbce97460f5e11b8
unknown <tanishque30@gmail.com>
2026-09-21 15:32:55 +0530

    node

--- 2ndYear/Day7.js
+++ 2ndYear/Day7.js
@@ -1,14 +1,56 @@
-class student{
-    constructor(){
-        console.log("Hello")
+// class student{
+//     constructor(hi){
+//         console.log(hi)
+//     }
+//     name1(){
+//         console.log("Vikas")
+//     }
+// }
+// class Tanishque extends student{
+//     constructor(){
+//         super("Hello")
+//         console.log("Hi")
+//     }
+//     static name=['Tanishque']
+//     name1(){
+//         for(let i in this.name)
+//             console.log(i)
+//     }
+// }
+// console.log(Tanishque.name)
+// let obj=new Tanishque()
+// obj.name1()
+class Employee{
+    name="";id=0;basic=0;
+    constructor(name,id,basic) {
+        this.name=name;
+        this.id=id;
+        this.basic=basic;
+    }
+    calculate(){
+        return this.basic
     }
 }
-class Tanishque extends student{
-    constructor(){
-        super()
-        console.log("Hi")
+class Manger extends Employee{
+    add=0
+    constructor(update,name,id,bsd)
+    {
+        super(name,id,bsd)
+        this.add=update
     }
-    static name=['Tanishque']
+    calculate(){
+        return this.basic+this.add
+    }
 }
-console.log(Tanishque.name)
-let obj=new Tanishque()
+let obj=new Manger(123,"Tanishque",12,9000)
+console.log("Update Salary",obj.calculate())
+
+function name(show)
+{
+    console.log("Hello")
+    setTimeout(()=>{console.log("Sarthak")},3000)
+    show()
+}
+name(()=>{
+    console.log("Tanishque")
+})
